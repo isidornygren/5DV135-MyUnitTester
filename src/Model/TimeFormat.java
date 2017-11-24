ab809942748f8fdd3cdc2a88ea1378e808d4c1f3
isidornygren <karlisidornygren@gmail.com>
2017-11-24 19:55:38 +0100

    Refactored timeformating to work as it should

--- src/Model/TimeFormat.java
+++ src/Model/TimeFormat.java
@@ -7,17 +7,22 @@ class TimeFormat {
      * @return a string formatted in x.xxxs format.
      */
     public static String seconds(double time) {
-        String string;
-        if (time > Math.pow(10, 9) * 60) { // Minutes
-            string = String.format("%.3fmin", time/(Math.pow(10, 9) * 60));
-        } else if (time > Math.pow(10, 7)) { // Seconds
-            string = String.format("%.3fs", time/(Math.pow(10, 9)));
+        String string = "";
+        if (time >= Math.pow(10, 9) * 60) { // Minutes
+            double minutes = time / (Math.pow(10, 9) * 60);
+            time = time % (Math.pow(10, 9) * 60);
+            string = String.format("%.3fmin", minutes);
+        }
+        if (time > Math.pow(10, 7)) { // Seconds
+            string += String.format("%.3fs", time/(Math.pow(10, 9)));
         } else if (time > Math.pow(10, 4)) { // Milliseconds
-            string = String.format("%.3fms", time/(Math.pow(10, 6)));
+            string += String.format("%.3fms", time/(Math.pow(10, 6)));
         } else if (time > 10) { // Microseconds
-            string = String.format("%.3fµs", time/(Math.pow(10, 3)));
-        } else{ // Nanosecond
-            string = String.format("%.3fns", time);
+            string += String.format("%.3fµs", time/(Math.pow(10, 3)));
+        } else if (time > 0){ // Nanosecond
+            string += String.format("%.3fns", time);
+        } else if (string == ""){
+            string = "0.000s";
         }
         return string;
     }
--- test/Model/TimeFormatTest.java
+++ test/Model/TimeFormatTest.java
@@ -7,6 +7,10 @@ import static org.junit.Assert.*;
 public class TimeFormatTest {
 
     @Test
+    public void minutes() throws Exception{
+        assertEquals("1.000min", TimeFormat.seconds(60000000000L));
+    }
+    @Test
     public void seconds() throws Exception{
         assertEquals("1.000s", TimeFormat.seconds(1000000000));
     }
@@ -19,5 +23,15 @@ public class TimeFormatTest {
     @Test
     public void milliseconds() throws Exception{
         assertEquals("1.000ms", TimeFormat.seconds(1000000));
+    }
+
+    @Test
+    public void microseconds() throws Exception{
+        assertEquals("1.000µs", TimeFormat.seconds(1000));
+    }
+
+    @Test
+    public void nanoseconds() throws Exception{
+        assertEquals("1.000ns", TimeFormat.seconds(1));
     }
 }
